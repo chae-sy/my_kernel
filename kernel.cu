3b69f04534bfdb3f0230ca690644ec55e224d5be
Seoyoon Chae <67832005+chae-sy@users.noreply.github.com>
2025-11-07 05:03:13 +0000

    initial commit

---
+++ kernel.cu
@@ -0,0 +1,13 @@
+__global__void reference_kernel(const half *A, const half *B, half *C, int M, int N, int K) {
+    int col = threadIdx.x + blockDim.x * blockIdx.x;
+    int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+    if ( row < M && col < N ) {
+        float acc = 0.f;
+        for (int i = 0; i< K; i++) {
+        acc += __half2float(A[row*K + i]) * __half2float(B[col + i*N]); }
+    C [row * N + col] = __float2half(acc);
+
+    }
+    
+}
